1b2fdfd617a6fe36eb1ef64ab5f313b9c3e45d3a
Matěj Vopálka <matej.vopalka@gmail.com>
2018-11-24 09:13:06 +0100

    Final file of raspberry pi python script

--- python_client/client.py
+++ python_client/client.py
@@ -1,3 +1,17 @@
 import urllib.request
-contents = urllib.request.urlopen("http://10.10.5.183/web/socket.php?name=5").read()
-print(contents)
+import time
+
+a = 3
+b = 7
+
+send = ("http://10.10.5.183/web/socket.php?data=" + str(a) + "&data2=" + str(b))
+print(send)
+
+while True:
+    send = ("http://10.10.5.183/web/socket.php?data=" + str(a) + "&data2=" + str(b))
+    print(send)
+    contents = urllib.request.urlopen(send).read()
+    print(contents)
+    a = a + 1
+    b = b + 2
+    time.sleep(5)
