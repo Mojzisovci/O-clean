4d01c26fcb41e29db8ef13a865aae751fd84d721
Aiphyth <45287483+Aiphyth@users.noreply.github.com>
2018-11-24 01:54:58 +0100

    Add files via upload

--- data.php
+++ data.php
@@ -5,6 +5,7 @@
  * Date: 23.11.2018
  * Time: 22:22
  */
+error_reporting(E_ERROR | E_PARSE);
 
 $data = [
     "test" => "testData"
@@ -15,7 +16,11 @@ $json = json_encode($data);
 print($json);
 
 $port = 30025;
-$hostname = "10.10.4.132";
-$socket = socket_create($socket, $hostname, SQL_TCP);
-
-if($socket === false) echo "Chyba ve vytvoreni socketu";
+$hostname = "10.10.4.149";
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+//var_dump(socket_connect($socket, $hostname, $port));
+$conn = socket_connect($socket, $hostname, $port);
+if($conn) {
+    //var_dump(socket_connect($socket, $hostname, $port));
+    socket_send($socket, $json, 100, null);
+}
