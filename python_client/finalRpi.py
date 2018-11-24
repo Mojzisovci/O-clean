fcd6cd1f2fbd817fafe9de29f6266b9041e6499c
Matěj Vopálka <39264845+mvopalka@users.noreply.github.com>
2018-11-24 10:48:16 +0100

    debug

--- python_client/finalRpi.py
+++ python_client/finalRpi.py
@@ -16,6 +16,9 @@ HOST = ''
 PORT = 30025
 ser = serial.Serial('/dev/ttyUSB0',9600)
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+
 
 def serWrite(data11, data22, data33, data44):
     send = ("http://10.10.5.183/web/socket.php?data=" + str(data11) + "&data2=" + str(data22)
@@ -41,27 +44,27 @@ def ardRead(data1,data2,data3,data4):
 
 
 
-def serRead():
+def serRead(s):
     conn = None
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.settimeout(5)
-        try:
-            s.listen()
-            conn, addr = s.accept()
-            #s.settimeout(5)
-        except socket.timeout as exce:
-            print(exce)
-        if(conn):
-            data = conn.recv(1024)
 
-            data = data.decode("utf-8")
-            data = json.loads(data)
-            data5 = (int(data['direction']) * 1000) + 2000
-            data6 = int(data['speed'])
-            data5 = data5 + data6
-            data5 = str(data5)
-            ardWrite(data5)
+    s.settimeout(5)
+    try:
+        s.listen()
+        conn, addr = s.accept()
+        #s.settimeout(5)
+    except socket.timeout as exce:
+        print(exce)
+    if(conn):
+        data = conn.recv(1024)
+
+        data = data.decode("utf-8")
+        data = json.loads(data)
+        data5 = (int(data['direction']) * 1000) + 2000
+        data6 = int(data['speed'])
+        data5 = data5 + data6
+        data5 = str(data5)
+        ardWrite(data5)
+
 
 def ardWrite(data5):
     ser.write(bytes(data5, 'utf-8'))
@@ -70,4 +73,4 @@ while True:
     print("Reading form arduino")
     data1, data2, data3, data4 = ardRead(data1, data2, data3, data4)
     print("Rading from server")
-    serRead()
+    serRead(s)
